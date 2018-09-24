3f7f7bb6bafdac61452812b488edfbf60482f89f
Tony Vu <vu.tony@gmail.com>
2018-09-24 14:15:11 -0700

    Refactor single lookup method into multiple

--- lib/puppet/functions/vault_lookup.rb
+++ lib/puppet/functions/vault_lookup.rb
@@ -3,60 +3,59 @@ Puppet::Functions.create_function(:vault_lookup) do
   dispatch :vault_lookup do
     param 'String', :path
     param 'String', :vault_url
-    optional_param 'Hash', :options
+    optional_param 'Boolean', :raise_exceptions
   end
 
-  def vault_lookup(path, vault_url, options = {})
-    default_options = { 'raise_exceptions' => true,
-                        'default_return_value' => 'vault_lookup_failure'
-                      }
+  def vault_lookup(path, vault_url, raise_exceptions = true)
+    _vault_lookup(path, vault_url)
+  rescue StandardError => e
+    raise if raise_exceptions
+    Puppet.err(e.message)
+    nil
+  end
 
-    merged_options = default_options.merge(options)
+  private
+
+  def _vault_lookup(path, vault_url)
     uri = URI(vault_url)
     # URI is used here to just parse the vault_url into a host string
     # and port; it's possible to generate a URI::Generic when a scheme
     # is not defined, so double check here to make sure at least
     # host is defined.
-    raise Puppet::Error.new("Unable to parse a hostname from #{vault_url}") unless uri.hostname
+    raise Puppet::Error, "Unable to parse a hostname from #{vault_url}" unless uri.hostname
 
-    connection = Puppet::Network::HttpPool.http_ssl_instance(uri.host,uri.port)
+    connection = Puppet::Network::HttpPool.http_ssl_instance(uri.host, uri.port)
 
-    response = connection.post('/v1/auth/cert/login',"")
-    unless response.kind_of?(Net::HTTPOK)
-      err_string = "Received #{response.code} response code from vault at #{uri.host} for authentication"
-      Puppet.err(err_string)
-      raise Puppet::Error.new(err_string) if merged_options[:raise_exceptions] == true
-      return default_options['default_return_value']
-    end
+    token = get_auth_token(connection)
 
-    begin
-      token = JSON.parse(response.body)['auth']['client_token']
-      raise Puppet::Error('No client_token found') if token == nil
-    rescue StandardError => e
-      err_string = "Unable to parse client_token from vault response, original exception from #{e.class} and message: #{e.message}"
-      Puppet.err(err_string)
-      raise Puppet::Error.new(err_string) if merged_options[:raise_exceptions] == true
-      return default_options['default_return_value']
-    end
-
-    secret_response = connection.get("/v1/#{path}", {"X-Vault-Token" => token} )
-    unless secret_response.kind_of?(Net::HTTPOK)
-      err_string = "Received #{secret_response.code} response code from vault at #{uri.host} for secret lookup"
-      Puppet.err(err_string)
-      raise Puppet::Error.new(err_string) if merged_options[:raise_exceptions] == true
-      return default_options['default_return_value']
+    secret_response = connection.get("/v1/#{path}", 'X-Vault-Token' => token)
+    unless secret_response.is_a?(Net::HTTPOK)
+      raise Puppet::Error, "Received #{secret_response.code} response code from vault at #{uri.host} for secret lookup"
     end
 
     begin
       data = JSON.parse(secret_response.body)['data']
-      raise Puppet::Error('No data found for given secret') if data == nil
-    rescue StandardError => e
-      err_string = "Unable to parse secret data from vault response, original exception from #{e.class} and message: #{e.message}"
-      Puppet.err(err_string)
-      raise Puppet::Error.new(err_string) if merged_options[:raise_exceptions] == true
-      return default_options['default_return_value']
+    rescue StandardError
+      raise Puppet::Error, "Error parsing json secret data from vault response"
     end
 
     Puppet::Pops::Types::PSensitiveType::Sensitive.new(data)
+  end
+
+  def get_auth_token(connection)
+    response = connection.post('/v1/auth/cert/login', '')
+    unless response.is_a?(Net::HTTPOK)
+      raise Puppet::Error, "Received #{response.code} response code from vault at #{uri.host} for authentication"
+    end
+
+    begin
+      token = JSON.parse(response.body)['auth']['client_token']
+    rescue StandardError
+      raise Puppet::Error, "Unable to parse client_token from vault response"
+    end
+
+    raise Puppet::Error, 'No client_token found' if token.nil?
+
+    token
   end
 end
