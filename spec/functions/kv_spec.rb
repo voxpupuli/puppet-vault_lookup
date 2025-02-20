7d715bf304d1a05e92dec9a5e9d76347006cae23
Tomas Barton <barton.tomas@gmail.com>
2025-02-20 10:44:03 +0100

    Accept options hash as second argument for vault_lookup::kv

--- spec/functions/kv_spec.rb
+++ spec/functions/kv_spec.rb
@@ -5,14 +5,14 @@ require 'spec_helper'
 describe 'vault_lookup::kv' do
   context 'with server address' do
     it {
-      exp = Puppet::Pops::Types::TypeFactory.deferred.create('vault_lookup::lookup', ['kv/data/secret', 'https://vault.example.com:8200'])
+      exp = Puppet::Pops::Types::TypeFactory.deferred.create('vault_lookup::lookup', ['kv/data/secret', { 'vault_addr' => 'https://vault.example.com:8200' }])
       # This kind_of matcher requires https://github.com/puppetlabs/rspec-puppet/pull/24
       expect(subject).to run.with_params(
-        'kv/data/secret', 'https://vault.example.com:8200'
+        'kv/data/secret'
       ).and_return(kind_of(Puppet::Pops::Types::PuppetObject))
 
       expect(subject).to run.with_params(
-        'kv/data/secret', 'https://vault.example.com:8200'
+        'kv/data/secret', { 'vault_addr' => 'https://vault.example.com:8200' }
       ).and_return(exp)
     }
   end
@@ -21,10 +21,30 @@ describe 'vault_lookup::kv' do
     let(:hiera_data) { { 'vault_lookup::server' => 'https://vault:8200' } }
 
     it {
-      exp = Puppet::Pops::Types::TypeFactory.deferred.create('vault_lookup::lookup', ['secret', 'https://vault:8200'])
+      exp = Puppet::Pops::Types::TypeFactory.deferred.create('vault_lookup::lookup', ['secret', {}])
 
       expect(subject).to run.with_params(
-        'secret', 'https://vault:8200'
+        'secret'
+      ).and_return(exp)
+    }
+  end
+
+  context 'with vault_addr key' do
+    it {
+      exp = Puppet::Pops::Types::TypeFactory.deferred.create('vault_lookup::lookup', ['secret', { 'vault_addr' => 'https://vault:8200' }])
+
+      expect(subject).to run.with_params(
+        'secret', { 'vault_addr' => 'https://vault:8200' }
+      ).and_return(exp)
+    }
+  end
+
+  context 'with field key' do
+    it {
+      exp = Puppet::Pops::Types::TypeFactory.deferred.create('vault_lookup::lookup', ['secret', { 'field' => 'password' }])
+
+      expect(subject).to run.with_params(
+        'secret', { 'field' => 'password' }
       ).and_return(exp)
     }
   end
