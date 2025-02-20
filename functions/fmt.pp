ad1d28194207f13e8760d06a99e73136d89962f1
Tomas Barton <barton.tomas@gmail.com>
2025-02-20 09:59:54 +0100

    fmt function
    Deferred sprint evaluation

---
+++ functions/fmt.pp
@@ -0,0 +1,13 @@
+# @summary Lazy format function
+# Will be evaluated on client side - not during catalog compile on server
+# @param format Ruby printf syntax
+# @param args arguments passed to sprintf function
+# @see https://idiosyncratic-ruby.com/49-what-the-format.html
+# @example
+#   vault_lookup::fmt("%<x>d + %<y>d = %<z>d", {'x' => 2, 'y' => 2, 'z' => 5})
+function vault_lookup::fmt(String $format, Hash $args) >> Deferred {
+  Deferred(
+    'sprintf',
+    [$format, $args]
+  )
+}
---
+++ spec/functions/fmt_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'vault_lookup::fmt' do
+  context 'deferred array format' do
+    it {
+      exp = Puppet::Pops::Types::TypeFactory.deferred.create('sprintf', ['foo=%<bar>d', {'bar' => 2}])
+      # This kind_of matcher requires https://github.com/puppetlabs/rspec-puppet/pull/24
+      expect(subject).to run.with_params(
+        'foo=%<bar>d', {'bar' => 2}
+      ).and_return(kind_of(Puppet::Pops::Types::PuppetObject))
+
+      expect(subject).to run.with_params(
+        'foo=%<bar>d', {'bar' => 2}
+      ).and_return(exp)
+    }
+  end
+end
